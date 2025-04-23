3b25485fcb6465b28476a58ee8a3c7448e748cf0
Aakash Singh <mail@singhaakash.dev>
2025-04-24 00:23:20 +0530

    cleanup css and add comments

--- src-tauri/src/inject/main.js
+++ src-tauri/src/inject/main.js
@@ -1,14 +1,25 @@
-const customStyles = `
+const css = String.raw;
+
+const customStyles = css`
   :root {
+    /* disable bouncy scroll */
     overscroll-behavior: none;
   }
 
+  body {
+    /* increase height of the navbar container elements */
+    --ytmusic-nav-bar-height: 80px;
+  }
+
   * {
+    /* disable text selection */
     user-select: none;
     -webkit-user-select: none;
 
-    cursor: default;
+    /* use default cursor to give a more native feel */
+    cursor: default !important;
 
+    /* hide scrollbars */
     &::-webkit-scrollbar {
       display: none !important;
     }
@@ -17,48 +28,53 @@ const customStyles = `
     }
   }
 
-  #layout {
-    --ytmusic-nav-bar-height: 80px;
-
-    > ytmusic-nav-bar {
-      padding-top: 40px;
-      padding-bottom: 20px;
-    }
+  ytmusic-menu-popup-renderer {
+    /* WIP: enable scrollbar for the custom right click menu */
   }
 
+  /* inset navbar elements for the traffic lights and taskbar */
+  #layout  > ytmusic-nav-bar {
+    padding-top: 40px;
+    padding-bottom: 20px;
+  }
+  #guide-renderer > div.guide-nav.style-scope.ytmusic-guide-renderer {
+    padding-top: 40px;
+    padding-bottom: 20px;
+  }
+
+  /* add blur effect to the player bar */
   #player-bar-background {
     background: rgba(0, 0, 0, 0.4) !important;
     backdrop-filter: blur(20px);
   }
-
   ytmusic-player-bar {
-    width:100vw !important;
+    /* make the player bar transparent */
     background: transparent !important;
+    /* make the player progress bar full width */
+    width:100vw !important;
+  }
+
+  /* WIP: make the expanding controls transparent and hide unwanted elements */
+  #expanding-menu {
+    background: transparent !important;
+  }
+  #right-controls:has(+ #expanding-menu[aria-hidden="false"]) > div {
+    display: none;
   }
 
   #player-page[is-mweb-modernization-enabled] {
+    /* increase the height of the expanded player bar m-ui*/
     --ytmusic-player-page-player-bar-height: 94px !important;
 
     &[player-page-open][is-mweb-modernization-enabled] {
+      &:not([is-tabs-view]) > div > #main-panel {
+        /* inset player page controls for traffic lights */
+        padding-top: 20px !important;
+      }
       > div > #top-player-bar {
+        /* inset the top player bar for traffic lights */
         padding-top: 30px !important;
       }
-
-      &:not([is-tabs-view]) > div > #main-panel {
-        margin-top: 20px !important;
-      }
-    }
-  }
-
-  div[data-tauri-drag-region] {
-    cursor: grab;
-    -webkit-app-region: drag;
-    user-select: none;
-    -webkit-user-select: none;
-
-    &:active {
-      cursor: grabbing;
-      cursor: -webkit-grabbing;
     }
   }
 `;
@@ -83,13 +99,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const tauri = window.__TAURI__;
   const appWindow = tauri.window.getCurrentWindow();
 
-  // const invisibleTitleBar = document.querySelector('div[data-tauri-drag-region]');
+  if (!document.querySelector("[data-tauri-decorum-tb]")) {
+    // if the taskbar is not already present, create it
+    let tbElHtml= `
+      <div data-tauri-decorum-tb="" style="top: 0px; left: 0px; z-index: 100; width: 100%; height: 32px;
+      display: flex; position: fixed; align-items: end; justify-content: end; background-color: transparent;">
+        <div data-tauri-drag-region="" style="width: 100%; height: 100%; background: none;"></div>
+      </div>`
+    document.body.insertAdjacentHTML('afterbegin', tbElHtml);
+  }
 
-  // invisibleTitleBar.addEventListener('dblclick', () => {
-  //   appWindow.isFullscreen().then(fullscreen => {
-  //     appWindow.setFullscreen(!fullscreen);
-  //   });
-  // });
 
   if (!window.__DEBUG_MODE__) {
     document.addEventListener('contextmenu', event => event.preventDefault());
